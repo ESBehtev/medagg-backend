5c327955523f84ae1fb126c546679e342b0e80b6
Lyosha <bogomolov2004alex@gmail.com>
2025-10-27 14:38:54 +0300

    devel: Setup PostgreSQL database

--- app/settings.py
+++ app/settings.py
@@ -68,6 +68,21 @@ TEMPLATES = [
 WSGI_APPLICATION = "app.wsgi.application"
 
 
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ["DB_ENGINE"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
